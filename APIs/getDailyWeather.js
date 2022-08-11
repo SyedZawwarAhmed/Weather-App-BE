fa784492e76385157c6a30234f1281d0fb3f9482
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-11 17:20:10 +0500

    DTO object used

--- APIs/getDailyWeather.js
+++ APIs/getDailyWeather.js
@@ -1,3 +1,7 @@
+import { threeHours, APPID } from "../common/constants";
+import { weekday } from "../common/enums";
+import { getDailyWeatherResponse } from "../DTOs/getDailyWeatherResponse";
+
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
@@ -5,33 +9,36 @@ const axios = require("axios");
 router.post("/getDailyWeather", async (req, res) => {
   try {
     const { city } = req.body;
-    const date = new Date()
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const date = new Date();
     const hours = date.getHours();
-    const threeHours = [0, 3, 6, 9, 12, 15, 18, 21]
-    let minDifference = 3
-    let currentHour = 0
+    let minDifference = 3;
+    let currentHour = 0;
     for (const threeHour of threeHours) {
-        const difference = Math.abs(threeHour - hours)
-        if (difference < minDifference) {
-            minDifference = difference
-            currentHour = threeHour;
-        }
+      const difference = Math.abs(threeHour - hours);
+      if (difference < minDifference) {
+        minDifference = difference;
+        currentHour = threeHour;
+      }
     }
 
     const dailyWeatherData = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=39cd5eb32b4a2c8bb9d707f7f0846c6a&units=metric`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${APPID}&units=metric`
     );
 
-    const currentHourForecast = dailyWeatherData.data.list.filter(day => parseInt(day.dt_txt.split(" ")[1].split(":")[0]) === currentHour)
-    res.send(currentHourForecast.map(day => ({
-        "temperature": day.main.temp,
-        "weather": day.weather[0].main,
-        "weatherIcon": day.weather[0].icon,
-        "date": day.dt_txt.split(" ")[0],
-        "day": weekday[new Date(day.dt_txt.split(" ")[0]).getDay()],
-        "time": day.dt_txt.split(" ")[1]
-    })));
+    const currentHourForecast = dailyWeatherData.data.list.filter(
+      (day) => parseInt(day.dt_txt.split(" ")[1].split(":")[0]) === currentHour
+    );
+    res.send(
+      currentHourForecast.map((day) => {
+        const temperature = day.main.temp
+        const weather = day.weather[0].main
+        const weatherIcon = day.weather[0].icon
+        const date = day.dt_txt.split(" ")[0]
+        const currentDay = weekday[new Date(day.dt_txt.split(" ")[0]).getDay()]
+        const time = day.dt_txt.split(" ")[1]
+        return new getDailyWeatherResponse(temperature, weather, weatherIcon, date, currentDay, time)
+      })
+    );
   } catch (error) {
     res.send(error.message);
   }

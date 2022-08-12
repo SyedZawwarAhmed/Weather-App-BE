b3b45db06081b3a642f18cd5b860ef6575759d2f
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-12 16:04:41 +0500

    imports fixed

--- APIs/getDailyWeather.js
+++ APIs/getDailyWeather.js
@@ -1,12 +1,15 @@
-import { APPID, FORECAST_URL, threeHours } from "../common/constants";
-import { weekday } from "../common/enums";
-import { getDailyWeatherResponse } from "../DTOs/getDailyWeatherResponse";
-
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+const getDailyWeatherResponse = require("../DTOs/getDailyWeatherResponse");
+const {APPID} = require("../common/constants");
+const {FORECAST_URL} = require("../common/constants");
+const {threeHours} = require("../common/constants");
+const weekday = require("../common/enums");
+
 router.post("/getDailyWeather", async (req, res) => {
+  console.log("hello");
   try {
     const { city } = req.body;
     const date = new Date();
@@ -22,10 +25,10 @@ router.post("/getDailyWeather", async (req, res) => {
     }
 
     const options = {
-      method: 'GET',
+      method: "GET",
       url: FORECAST_URL,
-      params: {q: city, APPID: APPID, units: 'metric'}
-    }
+      params: { q: city, APPID: APPID, units: "metric" },
+    };
     const dailyWeatherData = await axios.request(options);
 
     const currentHourForecast = dailyWeatherData.data.list.filter(
@@ -33,13 +36,20 @@ router.post("/getDailyWeather", async (req, res) => {
     );
     res.send(
       currentHourForecast.map((day) => {
-        const temperature = day.main.temp
-        const weather = day.weather[0].main
-        const weatherIcon = day.weather[0].icon
-        const date = day.dt_txt.split(" ")[0]
-        const currentDay = weekday[new Date(day.dt_txt.split(" ")[0]).getDay()]
-        const time = day.dt_txt.split(" ")[1]
-        return new getDailyWeatherResponse(temperature, weather, weatherIcon, date, currentDay, time)
+        const temperature = day.main.temp;
+        const weather = day.weather[0].main;
+        const weatherIcon = day.weather[0].icon;
+        const date = day.dt_txt.split(" ")[0];
+        const currentDay = weekday[new Date(day.dt_txt.split(" ")[0]).getDay()];
+        const time = day.dt_txt.split(" ")[1];
+        return new getDailyWeatherResponse(
+          temperature,
+          weather,
+          weatherIcon,
+          date,
+          currentDay,
+          time
+        );
       })
     );
   } catch (error) {

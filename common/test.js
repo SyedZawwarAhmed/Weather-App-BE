84357cb6e2ff1f4f2fa9ce48c157487a9fe2ace8
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-15 17:10:42 +0500

    chore: delete test.js

--- common/test.js
+++
@@ -1,5 +0,0 @@
-const date = new Date();
-const hours = date.getHours();
-const remainder = hours % 3;
-const result = remainder === 0 ? hours : remainder === 1 ? hours - 1 : hours + 1;
-console.log("🚀 ~ file: test.js ~ line 6 ~ result", result)

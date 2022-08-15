b6af4cbac5f5b87f0b3a6584b1ba345bed4e36ba
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-15 18:29:46 +0500

    update functions.js

--- common/functions.js
+++ common/functions.js
@@ -1,8 +1,17 @@
 const getHourDivisibleByThree = (date) => {
+  // The purpose of this function is to determine which hour is nearest to the hour in which the funciton is called.
+
   const hours = date.getHours();
   const remainder = hours % 3;
-  const currentHour =
-    remainder === 0 ? hours : remainder === 1 ? hours - 1 : hours + 1;
+  let currentHour = hours;
+
+  const CurrentHourIsNotDivisibleByThree = remainder !== 0;
+
+  if (CurrentHourIsNotDivisibleByThree) {
+    const isCurrentHourNearestToNextHour = remainder > 1;
+    if (isCurrentHourNearestToNextHour) currentHour += 1;
+    else currentHour -= 1;
+  }
   return currentHour;
 };
 

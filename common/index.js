0117681b54110d05fa6138bf5d2d5af745d72ff3
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-15 14:02:55 +0500

    updated

--- common/index.js
+++ common/index.js
@@ -1,16 +1,17 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-const express = require('express');
-
-const app = express()
+const express = require("express");
+const app = express();
 const port = 5000;
-
-const getDailyWeather = require('../APIs/getDailyWeather');
+const citiesRoute = require("./getCities");
+const dailyWeatherRoute = require("./getDailyWeather");
+const currentWeatherRoute = require("./getCurrentWeather");
+const doetenv = require("dotenv");
+doetenv.config();
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-app.use(express.json())
-app.use("/api", getDailyWeather)
+app.use(express.json());
+app.use("/api", citiesRoute);
+app.use("/api", dailyWeatherRoute);
+app.use("/api", currentWeatherRoute);

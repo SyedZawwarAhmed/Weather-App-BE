1e4cecceddfca9e9cbb1331164afe5afd19998e5
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-15 15:36:22 +0500

    update constants.js

--- common/constants.js
+++ common/constants.js
@@ -1,5 +1,4 @@
 const APPID = process.env.APPID;
 const FORECAST_URL = process.env.FORECAST_URL;
-const threeHours = [0, 3, 6, 9, 12, 15, 18, 21]
 
-module.exports = { APPID, FORECAST_URL, threeHours };
+module.exports = { APPID, FORECAST_URL };

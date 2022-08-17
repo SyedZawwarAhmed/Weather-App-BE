d9b98b639e9f51ea2ce5babd4dafd1c56837f960
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-17 12:38:35 +0500

    update getDailyWeather.js

--- APIs/getDailyWeather.js
+++ APIs/getDailyWeather.js
@@ -3,19 +3,18 @@ const router = express.Router();
 const axios = require("axios");
 
 const { WeatherExternalAPI } = require("../external_APIs/WeatherExternalAPI");
+const { getDailyWeatherResponse } = require("../DTOs");
 
 router.post("/getDailyWeather", async (req, res) => {
   try {
     const { city } = req.body;
 
-    
-
     const weatherExternalAPI = new WeatherExternalAPI();
     const data = await weatherExternalAPI.getDailyWeather(city);
-
+    const dailyWeatherData = new getDailyWeatherResponse(data);
 
     res.status(200).json({
-      data,
+      data: dailyWeatherData,
       error: null,
     });
   } catch (error) {

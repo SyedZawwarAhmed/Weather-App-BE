00b52137ef763ed11722dddf5403864b760e8bf3
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-17 10:52:30 +0500

    update WeatherExternalAPI.js

--- external_APIs/WeatherExternalAPI.js
+++ external_APIs/WeatherExternalAPI.js
@@ -1,12 +1,19 @@
 const axios = require("axios");
+const { weekday } = require("../common");
 const { APPID, FORECAST_URL } = require("../common/constants");
+const getHourDivisibleByThree = require("../common/functions");
+const { getDailyWeatherResponse } = require("../DTOs");
 
 class WeatherExternalAPI {
   FORECAST_URL;
   APPID;
+  currentHour;
+  weekday;
   constructor() {
     this.FORECAST_URL = FORECAST_URL;
     this.APPID = APPID;
+    this.currentHour = getHourDivisibleByThree(new Date());
+    this.weekday = weekday;
   }
   async getDailyWeather(city) {
     const options = {
@@ -15,7 +22,29 @@ class WeatherExternalAPI {
       params: { q: city, APPID: this.APPID, units: "metric" },
     };
     const dailyWeatherData = await axios.request(options);
-    return dailyWeatherData;
+    const currentHourForecast = dailyWeatherData.data.list.filter((day) => {
+        const currentHourResponse = parseInt(
+          day.dt_txt.split(" ")[1].split(":")[0]
+        );
+        return currentHourResponse === this.currentHour;
+      });
+      const data = currentHourForecast.map((day) => {
+        const temperature = day.main.temp;
+        const weather = day.weather[0].main;
+        const weatherIcon = day.weather[0].icon;
+        const date = day.dt_txt.split(" ")[0];
+        const currentDay = this.weekday[new Date(day.dt_txt.split(" ")[0]).getDay()];
+        const time = day.dt_txt.split(" ")[1];
+        return new getDailyWeatherResponse(
+          temperature,
+          weather,
+          weatherIcon,
+          date,
+          currentDay,
+          time
+        );
+      });
+    return data;
   }
 }
 

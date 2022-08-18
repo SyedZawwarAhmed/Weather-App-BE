01af47f7e0caf0bbca26c169133305bdbc383d42
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-18 15:11:32 +0500

    update common/index.js

--- common/index.js
+++ common/index.js
@@ -1,5 +1,6 @@
 const { APPID, FORECAST_URL } = require("./constants");
 const weekday = require("./enums");
+const BASE_URL = require("./constants")
 const getHourDivisibleByThree = require("./functions")
 
-module.exports = { APPID, FORECAST_URL, weekday, getHourDivisibleByThree };
+module.exports = { APPID, FORECAST_URL, weekday, BASE_URL, getHourDivisibleByThree };

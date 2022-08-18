bed5625f48eceed6466595aae9e8779dc7b275da
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-18 16:38:05 +0500

    test: update getDailyWeather.test.js

--- tests/getDailyWeather.test.js
+++ tests/getDailyWeather.test.js
@@ -1,4 +1,3 @@
-const request = require("supertest");
 const { BASE_URL } = require("../common/index");
 const axios = require("axios");
 jest.mock("axios");
@@ -6,37 +5,46 @@ jest.mock("axios");
 describe("GET /getDailyWeather", () => {
   const requestBody = { city: "Karachi" };
 
-  it("should return data of length 3" , async () => {
+  it("should return data of length 3", async () => {
     axios.post.mockResolvedValue({
-      data: [
-        {
-          temperature: 28.5,
-          weather: "Clouds",
-          weatherIcon: "04n",
-          date: "2022-08-17",
-          day: "Wednesday",
-          time: "15:00:00",
-        },
-        {
-          temperature: 26.77,
-          weather: "Rain",
-          weatherIcon: "10n",
-          date: "2022-08-18",
-          day: "Thursday",
-          time: "15:00:00",
-        },
-        {
-          temperature: 27.28,
-          weather: "Rain",
-          weatherIcon: "10n",
-          date: "2022-08-19",
-          day: "Friday",
-          time: "15:00:00",
-        },
-      ],
+      statusCode: 200,
+      body: {
+        data: [
+          {
+            temperature: 28.5,
+            weather: "Clouds",
+            weatherIcon: "04n",
+            date: "2022-08-17",
+            day: "Wednesday",
+            time: "15:00:00",
+          },
+          {
+            temperature: 26.77,
+            weather: "Rain",
+            weatherIcon: "10n",
+            date: "2022-08-18",
+            day: "Thursday",
+            time: "15:00:00",
+          },
+          {
+            temperature: 27.28,
+            weather: "Rain",
+            weatherIcon: "10n",
+            date: "2022-08-19",
+            day: "Friday",
+            time: "15:00:00",
+          },
+        ],
+        error: null,
+      }
     });
 
-    const response = await axios.post(`${BASE_URL}/api/getDailyWeather`, requestBody)
-    expect(response.data.length === 3).toBe(true);
+    const response = await axios.post(
+      `${BASE_URL}/api/getDailyWeather`,
+      requestBody
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.body.error).toBe(null);
+    expect(response.body.data.length === 3).toBe(true);
   });
 });

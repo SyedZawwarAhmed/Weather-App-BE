8de593e88c34ac11dda127926deacc88ed5bc306
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-22 12:49:22 +0500

    update getCities.js

--- APIs/getCities.js
+++ APIs/getCities.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const cities = require("all-the-cities");
+const { getCitiesResponse } = require("../DTOs");
 
 router.get("/getCities", (req, res) => {
   try {
-    res.json(cities.map(city => city.name));
+    res.json(cities.map((city) => new getCitiesResponse(city.name)));
   } catch (error) {
-    res.send(error.message)
+    res.send(error.message);
   }
 });
 

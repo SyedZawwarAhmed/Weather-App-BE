f25068a70bbc3eff195435cbd085c989d04d9308
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-29 15:59:11 +0500

    update getCurrentWeatherResponse.js

--- DTOs/getCurrentWeatherResponse.js
+++ DTOs/getCurrentWeatherResponse.js
@@ -1,16 +1,31 @@
-export class getCurrentWeatherResponse {
-    temperature
-    weather
-    weatherIcon
-    date
-    day
-    time
-    constructor(temperature, weather, weatherIcon, date, day, time) {
-        this.temperature = temperature;
-        this.weather = weather;
-        this.weatherIcon = weatherIcon;
-        this.date = date;
-        this.day = day;
-        this.time = time;
-    }
+class getCurrentWeatherResponse {
+  temperature;
+  weather;
+  weatherIcon;
+  date;
+  day;
+  time;
+  feelsLike;
+  humidity;
+  constructor(
+    temperature,
+    weather,
+    weatherIcon,
+    date,
+    day,
+    time,
+    feelsLike,
+    humidity
+  ) {
+    this.temperature = temperature;
+    this.weather = weather;
+    this.weatherIcon = weatherIcon;
+    this.date = date;
+    this.day = day;
+    this.time = time;
+    this.feelsLike = feelsLike;
+    this.humidity = humidity;
+  }
 }
+
+module.exports = getCurrentWeatherResponse ;

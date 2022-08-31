049febe8ed926e5b38016327aae40476f695b42a
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-31 14:07:30 +0500

    update index.js

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+var cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json());
 app.use("/api", getDailyWeather);
 app.use("/api", getCities)
